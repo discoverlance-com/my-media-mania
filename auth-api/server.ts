1b1b2b9142bcfb619d1994d49c609e8560183c34
lanarmah <lanarmah@gmail.com>
2024-08-03 18:35:56 +0000

    feat: update api to hash password

--- auth-api/server.ts
+++ auth-api/server.ts
@@ -1,4 +1,11 @@
+import * as bcrypt from 'https://deno.land/x/bcrypt@v0.3.0/mod.ts'
+
 const kv = await Deno.openKv()
+
+async function verifyPassword(password: string, hashedPassword: string) {
+	const isMatch = await bcrypt.compare(password, hashedPassword)
+	return isMatch
+}
 
 Deno.serve(async (request: Request) => {
 	const body = await request.json()
@@ -6,7 +13,10 @@ Deno.serve(async (request: Request) => {
 	const password = body.password
 
 	if (!password || !username) {
-		return new Response('Missing username or password', { status: 422 })
+		return new Response(
+			JSON.stringify({ message: 'Missing username or password' }),
+			{ status: 422 },
+		)
 	}
 
 	const result = await kv.get<{ password: string; id: string }>([
@@ -14,11 +24,25 @@ Deno.serve(async (request: Request) => {
 		username,
 	])
 
-	if (result && result.value && result.value.password === password) {
-		return new Response(JSON.stringify({ id: result.value.id }), {
-			status: 200,
-		})
+	if (result && result.value) {
+		// verify password
+		const isMatch = await verifyPassword(password, result.value.password)
+		if (!isMatch) {
+			return new Response(
+				JSON.stringify({ message: 'Invalid username or password' }),
+				{ status: 400 },
+			)
+		}
+		return new Response(
+			JSON.stringify({ id: result.value.id, message: 'Success' }),
+			{
+				status: 200,
+			},
+		)
 	} else {
-		return new Response('Invalid username or password', { status: 401 })
+		return new Response(
+			JSON.stringify({ message: 'Invalid username or password' }),
+			{ status: 400 },
+		)
 	}
 })
